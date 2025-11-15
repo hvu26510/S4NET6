ea84d4302dce6e9a9c614810ad9d06af6bb5a0f1
hvu26510 <hvu26510@gmail.com>
2025-11-15 08:06:25 +0700

    Add project files.

---
+++ S4NET6/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using S4NET6.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(
+    op => op.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ S4NET6/Services/JwtServices.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace S4NET6.Services
+{
+    public interface IJwtServices
+    {
+        string GenToken(string username, string role);
+    }
+
+    public class JwtServices : IJwtServices
+    {
+        private readonly IConfiguration _configuration;
+        public JwtServices(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenToken(string username, string role)
+        {
+            var jwtSection = _configuration.GetSection("Jwt");
+
+            //Tao khoa
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSection["Key"]));
+
+            //tao thong tin de sign
+            var credInfos = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //Claim
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            //Tao doi jwt
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSection["Issuer"],
+                audience: jwtSection["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(double.Parse(jwtSection["ExpireMinutes"])),
+                signingCredentials: credInfos
+                );
+            // tra ve chuoi token
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+    }
+}
